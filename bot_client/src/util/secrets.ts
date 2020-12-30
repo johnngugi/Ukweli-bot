5ac889824337556aea82410ed144453129583f56
John Ngugi <johnngugi777@gmail.com>
2020-12-31 00:48:54 +0300

    Add dockerfile

--- bot_client/src/util/secrets.ts
+++ bot_client/src/util/secrets.ts
@@ -5,12 +5,13 @@ import fs from 'fs';
 if (fs.existsSync('.env')) {
   logger.debug('Using .env file to supply config environment variables');
   dotenv.config({path: '.env'});
-} else {
-  logger.debug(
-    'Using .env.example file to supply config environment variables'
-  );
-  dotenv.config({path: '.env.example'}); // you can delete this after you create your own .env file!
 }
+// } else {
+//   logger.debug(
+//     'Using .env.example file to supply config environment variables'
+//   );
+//   dotenv.config({path: '.env.example'}); // you can delete this after you create your own .env file!
+// }
 
 export const ENVIRONMENT = process.env.NODE_ENV;
 // const prod = ENVIRONMENT === 'production'; // Anything else is treated as 'dev'
